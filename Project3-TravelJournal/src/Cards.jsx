a9975854efdf787eb8bbf56486519c4a8d3fc698
Mario Niko <marios-2222@hotmail.com>
2023-01-20 10:54:24 +0200

    style changes

--- Project3-TravelJournal/src/Cards.jsx
+++ Project3-TravelJournal/src/Cards.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { ImLocation } from "react-icons/Im";
 
 export default function Hero(props){
     return(
@@ -8,10 +8,10 @@ export default function Hero(props){
                 <img src={props.item.imageUrl} alt="" />
             </div>
             <div className="bodyDiv">
-                <span className="location">{props.item.location}</span>
-                <p className="gray inline">{props.item.googleMapsUrl}</p>
+                <span className="location"><ImLocation className="locIcon"/>{props.item.location}</span>
+                <a className="gray inline" href={props.item.googleMapsUrl}>View on Google Maps</a>
                 <h2>{props.item.title}</h2>
-                <p className="date">{props.item.startDate}</p>
+                <p className="date">{props.item.startDate} - {props.item.endDate}</p>
                 <p>{props.item.description}</p>
             </div>
         </div>
