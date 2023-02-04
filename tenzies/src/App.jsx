e05ec613080ecf6cdf4266ac645e90c37b8a06d8
Mario Niko <marios-2222@hotmail.com>
2023-02-04 17:59:07 +0200

    Select the dice you want to keep

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,31 +1,49 @@
 import React from "react";
-import ReactDom from "react-dom/client"
 import Die from "./Die";
+import {nanoid} from "nanoid"
 
 import "./App.css"
 
 export default function App(){
 
-function dieValue(){
-    return Math.floor(Math.random() * 6)+1
-
+    const [dice, setDice] = React.useState(allNewDice())
+    
+   const diceElements = dice.map(die => <Die
+    key = {die.id}
+    value = {die.value}
+    isHeld= {die.isHeld}
+    id = {die.id}
+    holdDice = {() => holdDice(die.id)}
+      />)
+  
+function allNewDice(){
+    const newDice = []
+        for(let i =0; i <10; i++){  
+            const obj = {value: Math.ceil(Math.random() * 6), isHeld: false, id:nanoid()}
+            newDice.push(obj)
+        }
+        return newDice
 }
 
+
+function holdDice(id){
+    setDice(prevState => {
+        return prevState.map(die => {
+            return die.id === id ? {...die, isHeld: !die.isHeld} : die
+        })
+    })
+}
+
+function rollDice(){
+    setDice(allNewDice())
+}
 
     return (
         <main >
             <div className="container">
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
-            <Die value= {dieValue()}/>
+            {diceElements}
             </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main> 
     )
 }
--- tenzies/src/Die.jsx
+++ tenzies/src/Die.jsx
@@ -4,10 +4,13 @@ import "./Die.css"
 
 
 export default function Die(props){
+const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white"
+}
 
     return (
-        <div className="dice">
-            <h2 className="die-face">{props.value}</h2>
+        <div style={styles} onClick={props.holdDice} className="die-face">
+            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
