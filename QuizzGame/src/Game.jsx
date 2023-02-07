70e7041b0a0ff0f86ad16d2e02129d756813d576
Mario Niko <marios-2222@hotmail.com>
2023-02-07 20:47:00 +0200

    display correct answer

--- QuizzGame/src/Game.jsx
+++ QuizzGame/src/Game.jsx
@@ -1,12 +1,13 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
 import "./Game.css"
 import "./Question"
 import Question from "./Question";
+
+
 export default function game(){
-
-
-
     const [questions, setQuestions] = useState([])
+    const [isGameOver, setIsGameOver] = useState(false)
 
     React.useEffect(()=>{
         async function getQuestions(){
@@ -19,15 +20,23 @@ export default function game(){
 
     const createGame = questions.map(q => (
         <Question
+        key={nanoid()}
+        id={nanoid()}
         q={q.question}
-        ans={[...q.incorrect_answers,q.correct_answer]}
+        ans={[...q.incorrect_answers,q.correct_answer].sort((a, b) => 0.5 - Math.random())}
+        isGameOver={isGameOver}
+        correct_answer={q.correct_answer}
         />
     ))
+
+    function gameOver(){
+        setIsGameOver(true)
+    }
 
     return(
         <section className="game-container">
             {createGame}
-                <button className="btn game-btn">Check Answers</button>
+                <button onClick={gameOver} className="btn game-btn">Check Answers</button>
         </section>
     )
 }
--- QuizzGame/src/Question.jsx
+++ QuizzGame/src/Question.jsx
@@ -2,14 +2,21 @@ import React from "react";
 
 
 export default function Question(props){
+    const style ={
+        backgroundColor: props.isGameOver && "#94D7A2"
+    }
+    const noStyle={
+        backgroundColor: props.isGameOver && ""
+    }
+    console.log(props.correct_answer)
     return (
         <section className="qBody">
                 <h2>{props.q}</h2>
             <div>
-                <p>{props.ans[0]}</p>
-                <p>{props.ans[1]}</p>
-                <p>{props.ans[2]}</p>
-                <p>{props.ans[3]}</p>
+                <p style={props.ans[0]===props.correct_answer ?style : noStyle}>{props.ans[0]}</p>
+                <p style={props.ans[1]===props.correct_answer ?style : noStyle}>{props.ans[1]}</p>
+                <p style={props.ans[2]===props.correct_answer ?style : noStyle}>{props.ans[2]}</p>
+                <p style={props.ans[3]===props.correct_answer ?style : noStyle}>{props.ans[3]}</p>
             </div>
         </section>
     )
